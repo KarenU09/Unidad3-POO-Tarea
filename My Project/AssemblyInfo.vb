b3b3a87bb84c3267fa19f7867fc0b3ba6bd2f386
KAREN ANNETTE UMAÑA DE LAZO <karenu91@gmail.com>
2022-12-03 17:43:39 -0600

    Agregar archivos de proyecto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("ISNP125022-ISNP406013-UNIDAD3")>
+<Assembly: AssemblyDescription("Tarea de la semana3 realizada por Karen Umaña-Wilber Viera")>
+<Assembly: AssemblyCompany("UGB")>
+<Assembly: AssemblyProduct("Formuarios para el uso de POO")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("KarenUmaña-WilberViera")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("f38d5b3e-f2da-4f88-95a9-809dcc458916")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
